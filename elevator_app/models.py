9f9ac6764f8e081faac2a96cb1b0835807a3e108
Gopallal575 <52308900+Gopallal575@users.noreply.github.com>
2023-04-03 12:06:07 +0530

    Update models.py

--- elevator_app/models.py
+++ elevator_app/models.py
@@ -1,36 +1,13 @@
 from django.db import models
 
-
 class Elevator(models.Model):
-    DIRECTION_UP = 'up'
-    DIRECTION_DOWN = 'down'
-    DIRECTION_CHOICES = [
-        (DIRECTION_UP, 'Up'),
-        (DIRECTION_DOWN, 'Down'),
-    ]
-
-    name = models.CharField(max_length=50, unique=True)
-    direction = models.CharField(max_length=4, choices=DIRECTION_CHOICES, default=DIRECTION_UP)
-    current_floor = models.PositiveIntegerField(default=1)
+    id = models.IntegerField(primary_key=True)
+    current_floor = models.IntegerField(default=0)
+    direction = models.CharField(max_length=10, default="up")
     is_operational = models.BooleanField(default=True)
 
-    @classmethod
-    def assign_elevator(cls, floors):
-        available_elevators = cls.objects.filter(is_operational=True)
-        if not available_elevators.exists():
-            return None
-
-        min_distance = None
-        selected_elevator = None
-        for elevator in available_elevators:
-            distance = abs(elevator.current_floor - floors[0])
-            for floor in floors[1:]:
-                distance += abs(elevator.current_floor - floor)
-            if min_distance is None or distance < min_distance:
-                min_distance = distance
-                selected_elevator = elevator
-
-        return selected_elevator
+    def __str__(self):
+        return f"Elevator {self.id}"
 
     @classmethod
     def get_elevator(cls, floor, direction):
@@ -44,4 +21,21 @@ class Elevator(models.Model):
             if (e.direction == direction and e.current_floor <= floor) or \
                     (e.direction != direction and e.current_floor >= floor):
                 distance = abs(e.current_floor - floor)
-                if min_distance is None or
+                if min_distance is None or distance < min_distance:
+                    min_distance = distance
+                    elevator = e
+
+        return elevator
+
+    def move(self):
+        if self.direction == "up":
+            self.current_floor += 1
+        else:
+            self.current_floor -= 1
+
+        if self.current_floor == 0:
+            self.direction = "up"
+        elif self.current_floor == 9:
+            self.direction = "down"
+
+        self.save()
